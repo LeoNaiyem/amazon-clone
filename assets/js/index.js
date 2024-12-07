9dec0f2321e22877a589b0aee49279b4e585a298
leonaiyem <mdnaiyem.islam.10@gmail.com>
2024-12-07 22:42:21 +0600

    added all products images

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+// owl carousel js code
+$(".owl-carousel").owlCarousel({
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  autoplay: true,
+  loop: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 6,
+    },
+  },
+});
